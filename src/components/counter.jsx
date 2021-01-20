9612fd72885aea045aac41e1c93e6236cfa69108
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-01-20 20:24:36 +0530

    Added arrow function instead of binding method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,12 +5,12 @@ class Counter extends Component {
     count: 0,
   };
 
-  constructor() {
-    super();
-    this.handleIncrement = this.handleIncrement.bind(this);
-  }
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
 
-  handleIncrement() {
+  handleIncrement = () => {
     console.log("Increment Clicked", this);
   }
 
