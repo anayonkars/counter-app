911a5a1650694919603190a028c32eec2f599f21
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-01-21 10:37:10 +0530

    Refactoring

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: this.props.value,
+    value: this.props.value,
   };
 
   // constructor() {
@@ -12,7 +12,7 @@ class Counter extends Component {
 
   handleIncrement = (product) => {
     console.log(product)
-    this.setState({count: this.state.count + 1});
+    this.setState({value: this.state.value + 1});
   };
 
   // doHandleIncrement = () => {
@@ -36,12 +36,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
